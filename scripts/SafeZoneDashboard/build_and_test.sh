ba0fa704b617faeb5527300d5a5951413fac3390
rebodutch <rebodutchman@gmail.com>
2025-01-07 15:51:35 +0800

    update test script

--- scripts/SafeZoneDashboard/build_and_test.sh
+++ scripts/SafeZoneDashboard/build_and_test.sh
@@ -24,40 +24,18 @@ if [ "$(docker ps -aq -f name="$CONTAINER_NAME")" ]; then
     docker rm "$CONTAINER_NAME"
 fi
 
-# step 5: intialize the database and export the db file
-# echo "Initializing the database..."
-# # if the db file already doesnt exists, then create it
-# if [ ! -f "$(pwd)/services/CovidDataIngestor/environments/test/db/test.db" ]; then
-#     docker run --rm \
-#     -v "$(pwd)/utils:/app/utils" \
-#     -v "$(pwd)/services/CovidDataIngestor/environments/test/db:/db" \
-#     --name "$CONTAINER_NAME" "$IMAGE_NAME:$IMAGE_TAG" \
-#     python3 /app/utils/db/init_db.py
-# fi
-
-
-# Step 6: run the tests in the container
+# Step 5: run the tests in the container
 # unit test
-# echo "Running unit tests..."
-# docker run --rm \
-#   -v "$(pwd)/utils:/app/utils" \
-#   --name "$CONTAINER_NAME" "$IMAGE_NAME:$IMAGE_TAG" \
-#   pytest test/unit_test
+echo "Running unit tests..."
+docker run --rm \
+  -v "$(pwd)/utils:/app/utils" \
+  --name "$CONTAINER_NAME" "$IMAGE_NAME:$IMAGE_TAG" \
+  pytest test/unit_test
 
-# # integration test
-# echo "Running integration tests..."
-# docker run --rm \
-#   -v "$(pwd)/services/CovidDataIngestor/environments/test/data:/data" \
-#   -v "$(pwd)/utils:/app/utils" \
-#   --name "$CONTAINER_NAME" "$IMAGE_NAME:$IMAGE_TAG" \
-#   pytest test/integration_test
-
-# echo "All tests passed successfully!"
-
-# # manual test
-echo "Running the container in interactive mode..."
+# manual test
+echo "Running the manual test..."
 docker run -it \
   -p 8050:8050 \
   -v "$(pwd)/utils:/app/utils" \
   --name "$CONTAINER_NAME" "$IMAGE_NAME:$IMAGE_TAG" \
-  python3 /app/main.py
+  python3 /test/manual_test/test.py
