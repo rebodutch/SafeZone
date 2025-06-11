ebbe8cedd54f218ac814af5974543104a13980e1
rebodutch <rebodutchman@gmail.com>
2025-06-11 02:10:51 +0000

    [REFACTOR][CLI][SCHEMA]: refactor schemas for CLI command and relay interaction

--- toolkit/cli/schemas/request.py
+++ toolkit/cli/schemas/request.py
@@ -1,16 +1,15 @@
 import datetime
 from typing import Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field # type: ignore
 
 # request data models
 class SimulateModel(BaseModel):
     date: datetime.date = Field(..., description="Invalid date format. Expected 'YYYY-MM-DD'.")
     end_date: Optional[datetime.date] = Field(None, description="Invalid date format. Expected 'YYYY-MM-DD'.")
-    dry_run: bool = Field(False, description="Dry run the simulation or not.")
 
 class VerifyModel(BaseModel):
     date: datetime.date = Field(..., description="Invalid date format. Expected 'YYYY-MM-DD'.")
-    interval: int = Field(..., description="Invalid interval format. Expected integer.")
+    interval: int = Field(1, description="Invalid interval format. Expected integer.")
     city: Optional[str] = Field(None, description="The length of city name is between 1 to 50.")
     region: Optional[str] = Field(None, description="The length of region name is between 1 to 50.")
     ratio: bool = Field(False, description="The data is ratio of population or not.")
--- toolkit/cli/schemas/response.py
+++ toolkit/cli/schemas/response.py
@@ -1,11 +1,41 @@
-import datetime
-from typing import Optional
-from pydantic import BaseModel, Field
+from datetime import datetime, timezone
+from typing import Generic, TypeVar, Optional
 
-# response models
+from pydantic import BaseModel, Field # type: ignore
+
+# --- data models  --- #
+class VerifyDataModel(BaseModel):
+    start_date: str = Field(
+        ..., description="Invalid date format. Expected 'YYYY-MM-DD'."
+    )
+    end_date: str = Field(
+        ..., description="Invalid date format. Expected 'YYYY-MM-DD'."
+    )
+    city: Optional[str] = Field(
+        None,
+        min_length=1,
+        max_length=50,
+        description="The length of city name is between 1 to 50.",
+    )
+    region: Optional[str] = Field(
+        None,
+        min_length=1,
+        max_length=50,
+        description="The length of region name is between 1 to 50.",
+    )
+    aggregated_cases: Optional[int] = Field(
+        None, ge=0, description="'cases' must be a positive integer."
+    )
+    cases_population_ratio: Optional[float] = Field(
+        None,
+        ge=0,
+        le=100,
+        description="'cases_population_ratio' must be a float between 0 and 1.",
+    )
 
 
-class ErrorDetail(BaseModel):
+# --- response model --- #
+class ErrorModel(BaseModel):
     fields: Optional[str] = Field(
         None, description="Short message describing the status."
     )
@@ -15,34 +45,16 @@ class ErrorDetail(BaseModel):
 class APIResponse(BaseModel):
     success: bool = Field(..., description="State of the response.")
     message: str = Field(..., description="Short message describing the status.")
-    errors: Optional[ErrorDetail] = Field(None, description="Error details.")
+    errors: Optional[ErrorModel] = Field(None, description="Error detail.")
+    # timestamp automatically generated with current UTC time in ISO 8601 format
+    timestamp: str = Field(
+        default_factory=lambda: datetime.now(timezone.utc).isoformat().replace("+00:00", "Z"),
+        description="UTC timestamp of response generation"
+    )
 
-
-class VerifyData(BaseModel):
-    date: datetime.date = Field(
-        ..., description="Invalid date format. Expected 'YYYY-MM-DD'."
-    )
-    interval: int = Field(..., description="Invalid interval format. Expected integer.")
-    city: Optional[str] = Field(
-        None, description="The length of city name is between 1 to 50."
-    )
-    region: Optional[str] = Field(
-        None, description="The length of region name is between 1 to 50."
-    )
-    cases: Optional[int] = Field(
-        None, ge=1, description="'cases' must be a positive integer."
-    )
-    ratio: Optional[float] = Field(
-        None, description="The data is ratio of population or not."
+class VerifyResponseModel(APIResponse):
+    data: VerifyDataModel = Field(
+        ..., description="Data field containing verification results."
     )
 
 
-class VerifyResponse(APIResponse):
-    data: VerifyData = Field(..., description="Verify Data details.")
-
-
-class PhaseResponse(APIResponse):
-    phase: str = Field(..., description="The current phase of the system.")
-    timestamp: datetime.datetime = Field(
-        ..., description="The timestamp of the phase checked."
-    )
