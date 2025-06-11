19074a382c697092ca82b407e977bc64a2fe30e5
rebodutch <rebodutchman@gmail.com>
2025-06-11 02:06:55 +0000

    [FEAT][CLI]: add log trace id support

--- toolkit/cli/relay/config/logger.py
+++ toolkit/cli/relay/config/logger.py
@@ -1,39 +1,40 @@
 # app/config/logger.py
+import sys
 import logging
-from logging.handlers import RotatingFileHandler
+import contextvars
+from logging import StreamHandler
 
-from config.settings import LOG_LEVEL, MAX_LOG_FILE_SIZE
-from config.settings import SERVICE_NAME
+from pythonjsonlogger import jsonlogger # type: ignore
 
-logger = logging.getLogger(SERVICE_NAME)
+from config.settings import LOG_LEVEL
 
-def setup_handlers():
-    # Set the log level based on the LOG_LEVEL environment variable [DEBUG, INFO, WARNING]
-    if LOG_LEVEL == "DEBUG":
-        logger.setLevel(logging.DEBUG)
-    elif LOG_LEVEL == "INFO":
-        logger.setLevel(logging.INFO)
-    elif LOG_LEVEL == "WARNING":
-        logger.setLevel(logging.WARNING)
-    else:
-        logger.setLevel(logging.INFO)
+trace_id_var = contextvars.ContextVar("trace_id", default="-")
 
-    file_handler = RotatingFileHandler(
-        f"{SERVICE_NAME}.log", maxBytes=MAX_LOG_FILE_SIZE, backupCount=3
+class TraceIdFilter(logging.Filter):
+    def filter(self, record):
+        record.trace_id = trace_id_var.get()
+        return True
+
+def setup_logger():
+    logger = logging.getLogger()
+    logger.setLevel(LOG_LEVEL)
+
+    # for loki JSON logs
+    json_handler = StreamHandler(sys.stdout)
+    json_formatter = jsonlogger.JsonFormatter(
+        '[%(trace_id)s] %(asctime)s %(levelname)s %(name)s %(message)s'
     )
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    json_handler.setFormatter(json_formatter)
+    json_handler.addFilter(TraceIdFilter())
+    logger.addHandler(json_handler)
 
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
-
-if not logger.handlers:
-    setup_handlers()
-
-def get_logger():
-    return logger
+    # for human-readable logs
+    if logger.level == logging.DEBUG:
+        text_handler = logging.StreamHandler(sys.stderr)
+        text_formatter = logging.Formatter(
+            '%(asctime)s %(levelname)s %(name)s %(message)s'
+        )
+        text_handler.setFormatter(text_formatter)
+        logger.addHandler(text_handler)
+    
+    logger.addFilter(TraceIdFilter())
--- toolkit/cli/relay/main.py
+++ toolkit/cli/relay/main.py
@@ -1,20 +1,43 @@
 # tools/cli/relay/app.py
-# third party library
-import uvicorn
-from fastapi import FastAPI
+import uuid
+import logging
 
-# local library
-from config.logger import get_logger
+import uvicorn # type: ignore
+from fastapi import FastAPI # type: ignore
+
+from config.logger import setup_logger, trace_id_var
 from config.settings import SERVER_IP, SERVER_PORT, SERVICE_NAME, SERVICE_VERSION
 from api.endpoints import router
 
+# Set up logging
+setup_logger()
+# Create FastAPI app instance
 app = FastAPI()
 # Register routers
 app.include_router(router)
-# Get the logger instance
-logger = get_logger()
+
+# ---- Middleware ----
+@app.middleware("http")
+async def add_trace_id(request, call_next):
+    """
+    Middleware to add trace_id to the request context.
+    This is useful for logging and tracing requests.
+    """
+    trace_id = request.headers.get("X-Trace-ID", str(uuid.uuid4()))
+    logger.debug(f"Received request with trace_id: {trace_id}")
+    
+    # Set the trace_id in the context variable
+    token = trace_id_var.set(trace_id)
+    
+    try:
+        response = await call_next(request)
+    finally:
+        trace_id_var.reset(token)  
+    response.headers["X-Trace-ID"] = trace_id
+    return response
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
     logger.info(f"Starting {SERVICE_NAME} version {SERVICE_VERSION}")
     uvicorn.run(app, host=SERVER_IP, port=SERVER_PORT)
