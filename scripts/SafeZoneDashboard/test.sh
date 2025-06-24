6b2937f0536f25a0d99b91575c803e39d6631afd
rebodutch <rebodutchman@gmail.com>
2025-06-24 08:40:20 +0000

    [refactor][script][time-server] support time server

--- scripts/SafeZoneDashboard/test.sh
+++ scripts/SafeZoneDashboard/test.sh
@@ -4,16 +4,15 @@ set -e
 # Step 1: set variables
 IMAGE_NAME="safezone_dashboard"
 IMAGE_TAG="test"
-CONTAINER_NAME="safezone_dashboard"
-DOCKERFILE_PATH="./services/SafeZoneDashboard/environments/test"
-TEST_PATH="./services/SafeZoneDashboard/environments/test/tests"
+CONTAINER_NAME="safezone_dashboard_test"
+DOCKERFILE_PATH="./services/SafeZoneDashboard"
 
 # Step 2: compile
 echo "No explicit compilation step required for Python..."
 
 # Step 3: build docker image
 echo "Building Docker image..."
-docker build --quiet -t "$IMAGE_NAME:$IMAGE_TAG" -f "$DOCKERFILE_PATH/Dockerfile.test" .
+docker buildx build -t "$IMAGE_NAME:$IMAGE_TAG" -f "$DOCKERFILE_PATH/Dockerfile.test" .
 
 # Step 4: stop the container if it is running from previous scripts
 if [ "$(docker ps -aq -f name="$CONTAINER_NAME")" ]; then
--- scripts/toolkit/TimeServer/build.sh
+++ scripts/toolkit/TimeServer/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Not implemented yet." 
+# Step 1: set variables
+IMAGE_NAME="safezone_time_server"
+IMAGE_TAG="latest" 
+DOCKERFILE_PATH="./toolkit/time-server"
+
+# Step 2: compile
+echo "No explicit compilation step required for Python..."
+
+# Step 3: build docker image
+echo "Building Docker image..."
+docker buildx build -t "$IMAGE_NAME:$IMAGE_TAG" -f "$DOCKERFILE_PATH/Dockerfile" .
